0d588fb5f60c4472a86b2db43aee88724f329780
krasikovap <krasikovap@gmail.com>
2017-02-13 22:44:24 +0600

    required fields in reservation

--- allrave/resource/js/reservation_form.js
+++ allrave/resource/js/reservation_form.js
@@ -23,6 +23,7 @@ $(document).ready(function () {
 
     function validation(fields){
         var valid = true;
+        $('.error_message ul li').remove();
         $.each(fields,function(i, val){
             var field =  $("#" + val);
 
